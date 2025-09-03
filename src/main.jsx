60eb10937049e996d1f80c2b47cc91663ad5c399
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-03 09:08:54 +0200

    fix: remove hardcoded basename from BrowserRouter for improved flexibility

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/stinelock.github.io/"}>
+    <BrowserRouter >
     <App />
     </BrowserRouter>
   </StrictMode>,
