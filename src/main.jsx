33e1658b42bdd20775854e307122d263216414e5
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-03 09:24:30 +0200

    fix: simplify BrowserRouter implementation by removing dynamic basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter
-      basename={import.meta.env.DEV ? "/" : "/stinelock.github.io/"}
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
