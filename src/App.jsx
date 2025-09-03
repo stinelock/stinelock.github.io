51fb47cbd71f2ec91013346173f70909cccc1bbb
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-03 09:57:13 +0200

    fix: remove unused App.css and clean up imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Routes, Route } from "react-router";
-import "./App.css";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactPage from "./pages/ContactPage";
