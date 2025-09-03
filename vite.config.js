98b0a2ca98c70f93cdcf3ee285f67eeed13d2fe3
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-03 09:51:02 +0200

    fix: simplify Vite config by removing unnecessary conditional base path

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: "/",
-  };
-
-  if (command !== "serve") {
-    config.base = "/"; // 👈 Replace with your repository name
-  }
-
-  return config;
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
 });
