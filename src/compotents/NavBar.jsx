c24e7c725324c6e7d20fdd19d5ed5cd052fc61bc
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-12 11:56:13 +0200

    feat: implement responsive mobile navigation and update styles for accessibility

--- src/compotents/NavBar.jsx
+++ src/compotents/NavBar.jsx
@@ -1,12 +1,37 @@
-import { NavLink }  from 'react-router';
+import { useState } from "react";
+import { NavLink } from "react-router";
 
 export default function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+
     return (
-        <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/project">Projects</NavLink>
+      <>
+        <nav className="mobile-nav">
+          <div className="burger-menu">
+            <i className="nav-btn" on onClick={toggleMenu}></i>
+          </div>
+
+          <div className={`menu ${isOpen ? "open" : "closed"}`}>
+            <NavLink to="/">
+              <img src="" alt="Hjem" />
+            </NavLink>
+            <NavLink to="/about">LEGEPLADS</NavLink>
+            <NavLink to="/project">PROJEKTER</NavLink>
+            <NavLink to="/contact">KONTAKT MIG</NavLink>
+          </div>
         </nav>
+        <nav className="desktop-nav">
+            <NavLink to="/">
+              <img src="" alt="Hjem" />
+            </NavLink>
+            <NavLink to="/about">LEGEPLADS</NavLink>
+            <NavLink to="/project">PROJEKTER</NavLink>
+            <NavLink to="/contact">KONTAKT MIG</NavLink>
+        </nav>
+      </>
     );
+  };
 }
