996c8239c8dfdbe28bc875ce87565160884ddea3
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-12 16:39:05 +0200

    feat: update Footer component with new links and improve styles for better UX

--- src/compotents/Footer.jsx
+++ src/compotents/Footer.jsx
@@ -1,7 +1,24 @@
+import arrowIcon from "/img/arrow-up-right.svg";
+
 export default function Footer() {
-    return (
-        <footer>
-        <p>&copy; 2024 My Portfolio</p>
-        </footer>
-    );
+  return (
+    <footer>
+      <p>&copy; 2025 My Portfolio</p>
+
+      <div className="link-container">
+        <a href="mailto:stine.b.lock@gmail.dk" className="footer-link">
+          EMAIL
+          <img src={arrowIcon} alt="Email Icon" />
+        </a>
+        <a href="https://github.com/stinelock">
+          GITHUB
+          <img src={arrowIcon} alt="GitHub Icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/stinelock">
+          LINKEDIN
+          <img src={arrowIcon} alt="LinkedIn Icon" />
+        </a>
+      </div>
+    </footer>
+  );
 }
