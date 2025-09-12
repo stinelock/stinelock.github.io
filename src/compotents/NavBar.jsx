688792a8e6a71cb922ccc4bdbb9d09188c1fc3b2
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-12 14:18:50 +0200

    feat: enhance NavBar with animated menu toggle and update styles for improved UX

--- src/compotents/NavBar.jsx
+++ src/compotents/NavBar.jsx
@@ -1,43 +1,57 @@
 import { useState } from "react";
 import { NavLink } from "react-router";
+import { motion, AnimatePresence } from "motion/react";
 
 export default function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen((prev) => !prev);
-}
+  };
 
-    return (
-      <>
-        <nav className="mobile-nav">
-          <div className="burger-menu">
-            <NavLink className="logo" to="/">
-              <img src="" alt="Hjem" />
-            </NavLink>
-            <span className="nav-btn" on onClick={toggleMenu}>
-              menu
-            </span>
-          </div>
+  const menuVariants = {
+    hidden: { opacity: 0, height: 0 },
+    visible: { opacity: 1, height: "auto" },
+  };
 
-          {isOpen && 
-            <div className="menulist">
+  return (
+    <>
+      <nav className="mobile-nav">
+        <div className="burger-menu">
+          <NavLink className="logo" to="/">
+            Logo
+          </NavLink>
+          <span className="nav-btn" onClick={toggleMenu}>
+            {isOpen ? "close" : "menu"}
+          </span>
+        </div>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="menulist"
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              variants={menuVariants}
+              transition={{ duration: 0.3, ease: "easeInOut" }}
+            >
               <NavLink to="/about">LEGEPLADS</NavLink>
               <hr />
               <NavLink to="/project">PROJEKTER</NavLink>
               <hr />
               <NavLink to="/contact">KONTAKT MIG</NavLink>
-            </div>}
-        </nav>
-        <nav className="nav-desktop">
-          <NavLink to="/">
-            <img src="" alt="Hjem" />
-          </NavLink>
-          <NavLink to="/about">LEGEPLADS</NavLink>
-          <NavLink to="/project">PROJEKTER</NavLink>
-          <NavLink to="/contact">KONTAKT MIG</NavLink>
-        </nav>
-      </>
-    );
-  };
-
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </nav>
+      <nav className="nav-desktop">
+        <NavLink to="/">
+          <img src="" alt="Hjem" />
+        </NavLink>
+        <NavLink to="/about">LEGEPLADS</NavLink>
+        <NavLink to="/project">PROJEKTER</NavLink>
+        <NavLink to="/contact">KONTAKT MIG</NavLink>
+      </nav>
+    </>
+  );
+}
