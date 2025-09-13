58b25eacb68fc50b662345a93e4c97c15545dabf
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-13 20:34:06 +0200

    feat: implement conditional rendering for contact details in ContactSection

--- src/compotents/ContactSection.jsx
+++ src/compotents/ContactSection.jsx
@@ -3,6 +3,8 @@ import ContactCTA from "./ContactCTA";
 export default function ContactSection() {
   return (
     <section className="contact-section">
+
+        {/* Jeg tænker at jeg i den her sektoin kan lave en conditional rendering hvor mit tlf og email skal vises hvis man er på "/Contact" params?? og CTA knap hvis det er på forsiden*/}
         
       <h2>Kontakt mig</h2>
       <ContactCTA />
