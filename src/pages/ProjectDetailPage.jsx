c29b3eba1772293dcaaea4833bdc7b923c4e1157
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-14 15:22:06 +0200

    feat: enhance ProjectDetailPage layout and styles for better presentation of project information

--- src/pages/ProjectDetailPage.jsx
+++ src/pages/ProjectDetailPage.jsx
@@ -31,8 +31,18 @@ export default function ProjectDetailPage() {
             <p>{project.year}</p>
           </div>
         </div>
-
         <p>{project.description}</p>
+        <div className="project-info" id="fokus">
+          <p>FOKUS</p>
+          <ul className="tags-list">
+            {project.tags?.map((tag, index) => (
+              <li key={index}>{tag}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="img-container">
+        <img src={project.image} alt={project.title}></img>
       </div>
     </main>
   );
