e1af76b543194aacad2168ed0c40e1dd0ceda104
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-15 11:00:00 +0200

    feat: update PlaygroundPage layout and styles for improved presentation and add image handling

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -1,10 +1,15 @@
-export default function About() {
+export default function PlaygroundPage() {
   return (
     <>
-      <main className="page">
-        <h1>Legeplads</h1>
-        <p>Jeg arbejder i mere end skærmstørrelser og pixels</p>
+      <main className="page-playground">
+        <section className="intro-playground">
+          <h1>Legeplads</h1>
+          <p>Jeg arbejder i mere end skærmstørrelser og pixels</p>
+        </section>
         <section className="project-imgs">
+          <div className="img-container">
+            <img></img>
+          </div>
           <div className="img-container">
             <img></img>
           </div>
@@ -13,6 +18,9 @@ export default function About() {
           </div>
         </section>
         <section className="project-imgs">
+          <div className="img-container">
+            <img></img>
+          </div>
           <div className="img-container">
             <img></img>
           </div>
@@ -22,7 +30,9 @@ export default function About() {
         </section>
         <section className="instagram">
           <p>Flere analoge kreationer</p>
-          <a href=""></a>
+          <a href="https://www.instagram.com/stines.streger?utm_source=ig_web_button_share_sheet&igsh=Mms4MmM5d2N0MHM1">
+            <img src="/img/insta-icon.svg"></img>
+          </a>
         </section>
       </main>
     </>
