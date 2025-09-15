82a4ffe8e7057113b3fcb1a1268070aa6c72ef43
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-15 10:09:51 +0200

    feat: update PlaygroundPage with new content and add Instagram icon

--- src/pages/PlaygroundPage.jsx
+++ src/pages/PlaygroundPage.jsx
@@ -1,11 +1,30 @@
-
-
 export default function About() {
-    return (
-      <>
-        <main className="page">
-          <h1>Legeplads</h1>
-        </main>
-      </>
-    );
+  return (
+    <>
+      <main className="page">
+        <h1>Legeplads</h1>
+        <p>Jeg arbejder i mere end skærmstørrelser og pixels</p>
+        <section className="project-imgs">
+          <div className="img-container">
+            <img></img>
+          </div>
+          <div className="img-container">
+            <img></img>
+          </div>
+        </section>
+        <section className="project-imgs">
+          <div className="img-container">
+            <img></img>
+          </div>
+          <div className="img-container">
+            <img></img>
+          </div>
+        </section>
+        <section className="instagram">
+          <p>Flere analoge kreationer</p>
+          <a href=""></a>
+        </section>
+      </main>
+    </>
+  );
 }
