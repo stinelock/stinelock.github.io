8cf76c4a590d82f864d49432f2e6a582a822482b
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-15 09:24:57 +0200

    feat: add ScrollToTop component and update layout and styles for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import { Routes, Route, useLocation } from "react-router";
+import { Routes, Route, Router, useLocation } from "react-router";
 import HomePage from "./pages/HomePage";
 import PlaygroundPage from "./pages/PlaygroundPage";
 import ContactPage from "./pages/ContactPage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
 import NavBar from "./compotents/NavBar";
 import Footer from "./compotents/Footer";
+import ScrollToTop from "./compotents/ScrollToTop";
 
 function App() {
   return (
     <>
+    <ScrollToTop />
       <NavBar />
       <Routes>
         <Route path="/" element={<HomePageWrapper />} />
