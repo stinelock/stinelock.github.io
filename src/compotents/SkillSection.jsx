607c3db92692e982c05bb0dd7d508961711d22d1
stinelock <eaa24stlo@students.eaaa.dk>
2025-09-22 18:56:42 +0200

    feat: correct spelling of AfterEffects and clean up CSS formatting

--- src/compotents/SkillSection.jsx
+++ src/compotents/SkillSection.jsx
@@ -7,7 +7,7 @@ const skills = [
   { text: "Figma", top: "30%", left: "50%", speed: -60 },
   { text: "Photoshop", top: "30%", left: "10%", speed: 100 },
   { text: "Illustrator", top: "62%", left: "50%", speed: -90 },
-  { text: "AfterEffect", top: "55%", left: "30%", speed: 70 },
+  { text: "AfterEffects", top: "55%", left: "30%", speed: 70 },
 ];
 export default function SkillPills() {
   const { scrollYProgress } = useScroll();
